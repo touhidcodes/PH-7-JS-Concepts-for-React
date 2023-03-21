dabca8451410bdba54a960e8ddd79389c75548dd
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-22 00:16:08 +0600

    notation

---
+++ notation.js
@@ -0,0 +1,14 @@
+const student = {
+	name: "touhid",
+	age: 24,
+	id: 24,
+	numbers: [23, 24, 56, 78],
+};
+
+// 1. Dot notation
+const name = student.name;
+
+// 2. Bracket notation
+const id = student["id"];
+const num = "numbers";
+const numbs = student[num];
